49d504fb21978d8390363f6e345221a4594856ff
akostishchina <sasha.kostischina98@gmail.com>
2020-03-13 21:48:22 +0300

    home

---
+++ index.js
@@ -0,0 +1,15 @@
+const fruits = [
+    {id: 1, title: 'Яблоки', price: 20}
+    {id: 2, title: 'Апельсины', price: 30}
+    {id: 3, title: 'Манго', price: 40}
+]
+
+const modal = $.modal({
+    title: 'V Modal',
+    closable: true,
+    content: `
+    <h4>Modal is working</h4>
+    <p>cbdbcuf c fubucd cbudb dhbsu hbfdybcs fcdsd</p>
+`
+    width: '400px'
+                      })
